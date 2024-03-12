a2f5c2dc57d0380e96215c57785af73733e976fd
lorrso <anreich0@gmail.com>
2024-03-12 20:13:42 +0300

    Домашнее задание было обновлено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,33 +7,30 @@ function solveEquation(a, b, c) {
 	let xSecond;
 	let d = b ** 2 - 4 * a * c;
 
-	switch (d) {
-		case d < 0:
-			arr = [];
-			break;
-
-		case d = 0:
-			x = -b / (2 * a);
-			arr.push(x);
-			break;
-
-		case d > 0:
-			xFirst = (-b + Math.sqrt(d)) / (2 * a);
-			arr.push(xFirst);
-			xSecond = (-b - Math.sqrt(d)) / (2 * a);
-			arr.push(xSecond);
-			break;
+	if (d < 0) {
+		arr = [];
+	} else if (d = 0) {
+		x = -b / (2 * a);
+		arr.push(x);
+	} else if (d > 0) {
+		xFirst = (-b + Math.sqrt(d)) / (2 * a);
+		arr.push(xFirst);
+		xSecond = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(xSecond);
 	}
+
 	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let p = percent ** -100 / 12;
+	let p = (percent / 12) ** -100;
+	p = p.toFixed(2);
+	typeof p === "number";
 	let s = amount - contribution;
 	let n = countMonths;
-	let payment = (s * (p + (p / (((1 + p) ** n) - 1)))) * n;
-	payment = payment.toFixed(2);
-	typeof payment === "number";
+	let totalMortgage = (s * (p + (p / (((1 + p) ** n) - 1)))) * n;
+	totalMortgage = totalMortgage.toFixed(2);
+	typeof totalMortgage === "number";
 
-	return payment;
+	return totalMortgage;
 }
