c90c2e1d2853f1b30a835c6805c6c83b560d27d8
lorrso <anreich0@gmail.com>
2024-03-24 21:12:14 +0300

    Вторая задача отредактирована

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -9,6 +9,6 @@ function getUsersNamesInAgeRange(users, gender) {
 	let filteredUsers = users.filter(user => user.gender === gender);
 	if (filteredUsers.length === 0) return 0;
     let result = filteredUsers.reduce((acc, user) => acc + user.age);
-	const avgAge = result / filteredUsers.length;
-	return avgAge;
+	const averageAge = result / filteredUsers.length;
+	return averageAge;
 }
