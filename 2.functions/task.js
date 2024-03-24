fcd2db0783e809b15e223b8df393073fc28e5851
lorrso <anreich0@gmail.com>
2024-03-24 12:58:10 +0300

    Домашнее задание отредактировано

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -22,7 +22,7 @@ function getArrayParams(...arr) {
 
 function summElementsWorker(...arr) {
 	if (arr.length === 0) return 0;
-	let sum = arr[0];
+	let sum = 0;
 	for (let i = 0; i < arr.length; i++) {
 		sum += arr[i];
 	}
@@ -33,7 +33,6 @@ function differenceMaxMinWorker(...arr) {
 	if (arr.length === 0) return 0;
 	let min = arr[0];
 	let max = arr[0];
-	const difference = max - min;
 	for (let i = 0; i < arr.length; i++) {
 		if (min > arr[i]) {
 			min = arr[i];
@@ -42,6 +41,7 @@ function differenceMaxMinWorker(...arr) {
 			max = arr[i];
 		}
 	}
+  const difference = max - min;
 	return difference;
 }
 
@@ -56,13 +56,12 @@ function differenceEvenOddWorker(...arr) {
 		} else {
 			sumOddElement += arr[i];
 		}
-
+	}
 		if (sumEvenElement > sumOddElement) {
 			difference = sumEvenElement - sumOddElement;
 		} else {
 			difference = sumOddElement - sumEvenElement;
 		}
-	}
 	return difference;
 }
 
