d4c4cb1ce2020e39456df58aeb5bb701a35807fb
lorrso <anreich0@gmail.com>
2024-03-24 20:49:10 +0300

    Домашнее задание добавлено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1 !== arr2) return false;
+	return arr1.every((value, index) => {
+		return arr1[index] === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = arr.filter(user => user.gender === gender).reduce((acc, user) => acc + user.age);
+	if (result.length === 0) return 0;
+	const avgAge = result / result.length;
+	return avgAge;
 }
