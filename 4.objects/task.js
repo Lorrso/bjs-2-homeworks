ff2876589de5b05c11e024660283e7580945d7f1
lorrso <anreich0@gmail.com>
2024-03-27 11:40:28 +0300

    Домашнее задание добавлено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (!this.marks) {
+		console.log("Добавление оценок невозможно. Студент отчислен.");
+		return;
+	}
+	this.marks.push(...marksToAdd);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks) return 0;
+	const sum = this.marks.reduce((acc, marks) => acc + marks, 0);
+	const avg = sum / this.marks.length;
+	return avg;
 }
-
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.exclude = reason;
 }
